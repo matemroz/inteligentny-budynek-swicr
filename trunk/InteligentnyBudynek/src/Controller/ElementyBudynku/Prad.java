3dea499945b4692733aeb8b87f84c5db3aa62457
bieniek.greg@gmail.com <bieniek.greg@gmail.com@5c68c409-c285-7ed9-41a2-fddc0483fbd3>
2011-04-02 21:07:54 +0000

    testowo dodano Prad.java

--- trunk/InteligentnyBudynek/src/Controller/ElementyBudynku/Prad.java
+++ trunk/InteligentnyBudynek/src/Controller/ElementyBudynku/Prad.java
@@ -1,23 +1,29 @@
 package Controller.ElementyBudynku;
 
+import Model.DAO.MsGazPradDAO;
+
 public class Prad {
 
 	public double cenaKWH;
 
 	public Prad(){
-
+            //TODO: co jesli nie uda sie pobrac ceny?
+            cenaKWH = (new MsGazPradDAO()).pobierzCenePradu();
 	}
 
 	public double getCenaKWH(){
-		return 0;
+            return this.cenaKWH;
 	}
 
-	/**
-	 * 
-	 * @param cenaKWH
-	 */
-	public void setCenaKWH(double cenaKWH){
+	
+	public boolean setCenaKWH(double cenaKWH){
+            //TODO: co jesli nie uda sie zmienic ceny w bazie?
+            if (cenaKWH < 0)
+                return false;
 
+            this.cenaKWH = cenaKWH;
+            (new MsGazPradDAO()).ustawCenePradu(cenaKWH);
+            return true;
 	}
 
 }
