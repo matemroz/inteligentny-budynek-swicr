135175cc24343005f967c35c131b6836b0eff291
bieniek.greg <bieniek.greg@gmail.com>
2011-04-03 17:44:20 +0000

    dodano 6 klas: Okno, Pokoj, Prad, PunktGrzewczy, PunktSwietlny, ZraszaczPrzeciwpozarowy

--- InteligentnyBudynek/src/Controller/ElementyBudynku/Pokoj.java
+++ InteligentnyBudynek/src/Controller/ElementyBudynku/Pokoj.java
@@ -1,40 +1,61 @@
-package ElementyBudynku;
+package Controller.ElementyBudynku;
 
+import Model.DAO.MsPokojDAO;
 import java.util.List;
 
 public class Pokoj {
 
-	private int idPietra;
 	private int idPokoju;
 	private String nazwa;
 	private List Urzadzenia;
 
-	public Pokoj(){
-
+	public Pokoj(int idPokoju){
+            this.idPokoju = idPokoju;
+            this.nazwa = (new MsPokojDAO()).pobierzNazwa(this.idPokoju);
 	}
 
-	public int getIdPietra(){
-		return 0;
+        public Pokoj(int idPokoju, String nazwa){
+            this.idPokoju = idPokoju;
+            this.nazwa = nazwa;
+            (new MsPokojDAO()).ustawNazwe(this.idPokoju, nazwa);
 	}
+
 
 	public int getIdPokoju(){
-		return 0;
+            return this.idPokoju;
 	}
 
 	public String getNazwa(){
-		return "";
+            return this.nazwa;
 	}
 
-	/**
-	 * 
-	 * @param nazwa
-	 */
 	public void setNazwa(String nazwa){
+            (new MsPokojDAO()).ustawNazwe(this.idPokoju, nazwa);
+            this.nazwa = nazwa;
+
+            /* //TODO: ustawNazwa powinna zwracac boolean
+               boolean ifSuccess = (new MsPokojDAO()).ustawNazwa(this.idPokoju, nazwa);
+            if (ifSuccess) {
+                this.nazwa = nazwa;
+            }
+            else {
+                System.out.println("Wystapił błąd podczas rejestrowania zmiany nazwy "
+                        + this.nazwa + " w bazie danych. Zmiana nie została zarejestrowana!");
+            }
+             */
 
 	}
 
 	public List wylistujUrzadzenia(){
-		return null;
+            return Urzadzenia;
 	}
+
+        public boolean dodajUrzadzenie(Urzadzenie u){
+            return this.Urzadzenia.add(u);
+        }
+
+        public boolean usunUrzadzenie(Urzadzenie u){
+            return this.Urzadzenia.remove(u);
+        }
 
 }
--- InteligentnyBudynek/src/Controller/ElementyBudynku/ZraszaczPrzeciwpozarowy.java
+++ InteligentnyBudynek/src/Controller/ElementyBudynku/ZraszaczPrzeciwpozarowy.java
@@ -1,49 +1,115 @@
 package Controller.ElementyBudynku;
 
+import Model.DAO.MsUrzadzenieDAO;
+
 public class ZraszaczPrzeciwpozarowy extends Urzadzenie {
 
 	private double moc;
 	private String nazwa;
 	private boolean pracuje;
+        private int idUrzadzenia;
+        private int idPokoju;
 
-	public ZraszaczPrzeciwpozarowy(){
-
+        public ZraszaczPrzeciwpozarowy(int idPokoju){
+            this.idPokoju = idPokoju;
+            this.idUrzadzenia = (new MsUrzadzenieDAO()).dodaj(this.idPokoju);
+            this.moc = 30;
+            (new MsUrzadzenieDAO()).ustawMoc(this.idUrzadzenia, moc);
+            this.nazwa = "Zraszacz przeciwpozarowy#"+idUrzadzenia;
+            (new MsUrzadzenieDAO()).ustawNazwa(this.idUrzadzenia, nazwa);
+            this.pracuje = false;
 	}
 
+	public ZraszaczPrzeciwpozarowy(int idPokoju,int idUrzadzenia){
+            this.idPokoju = idPokoju;
+            this.idUrzadzenia = idUrzadzenia;
+            this.moc = (new MsUrzadzenieDAO()).pobierzMoc(this.idUrzadzenia);
+            this.nazwa = (new MsUrzadzenieDAO()).pobierzNazwa(this.idUrzadzenia);
+            this.pracuje = false;
+	}
+
+        public ZraszaczPrzeciwpozarowy(int idPokoju,int idUrzadzenia, double moc){
+            this.idPokoju = idPokoju;
+            this.idUrzadzenia = idUrzadzenia;
+            this.moc = moc;
+            (new MsUrzadzenieDAO()).ustawMoc(this.idUrzadzenia, moc);
+            this.nazwa = (new MsUrzadzenieDAO()).pobierzNazwa(this.idUrzadzenia);
+            this.pracuje = false;
+	}
+
+   @Override
+        public int getIdPokoju(){
+            return this.idPokoju;
+        }
+
+    @Override
+        public int getIdUrzadzenia(){
+            return this.idUrzadzenia;
+        }
+
 	public boolean czyPracuje(){
-		return false;
+            return this.pracuje;
 	}
 
 	public double getMoc(){
-		return 0;
+            return this.moc;
 	}
 
+    @Override
 	public String getNazwa(){
-		return "";
+            return this.nazwa;
 	}
 
-	/**
-	 * 
-	 * @param moc
-	 */
-	public void setMoc(double moc){
+	public boolean setMoc(double moc){
+            if ( moc < 0 ) {
+                System.out.println("Podano ujemną moc urządzenia!");
+                return false;
+            }
 
+            boolean ifSuccess = (new MsUrzadzenieDAO()).ustawMoc(this.idUrzadzenia, moc);
+            if (ifSuccess) {
+                this.moc = moc;
+                return true;
+            }
+            else {
+                System.out.println("Wystapił błąd podczas rejestrowania zmiany mocy "
+                        + this.nazwa + " w bazie danych. Zmiana nie została zarejestrowana!");
+            }
+            return false;
 	}
 
-	/**
-	 * 
-	 * @param nazwa
-	 */
+    @Override
 	public void setNazwa(String nazwa){
-
+            boolean ifSuccess = (new MsUrzadzenieDAO()).ustawNazwa(this.idUrzadzenia, nazwa);
+            if (ifSuccess) {
+                this.nazwa = nazwa;
+            }
+            else {
+                System.out.println("Wystapił błąd podczas rejestrowania zmiany nazwy "
+                        + this.nazwa + " w bazie danych. Zmiana nie została zarejestrowana!");
+            }
 	}
 
 	public void wlacz(){
-
+            boolean ifSuccess = (new MsUrzadzenieDAO()).rejestrujWlaczenie(this.idUrzadzenia);
+            if (ifSuccess) {
+                this.pracuje = true;
+            }
+            else {
+                System.out.println("Wystapił błąd podczas rejestrowania włączenia "
+                        + this.nazwa + " w bazie danych. Włączenie nie zostało zarejestrowane!");
+            }
 	}
 
 	public void wylacz(){
-
+            boolean ifSuccess = (new MsUrzadzenieDAO()).rejestrujWlaczenie(this.idUrzadzenia);
+            if (ifSuccess) {
+                this.pracuje = true;
+            }
+            else {
+                System.out.println("Wystapił błąd podczas rejestrowania wyłączenia "
+                        + this.nazwa + " w bazie danych. Wyłączenie nie zostało zarejestrowane!");
+            }
 	}
 
 }
