3e55c6c976fdfe847b42ea523930ec21c0612375
matemroz@gmail.com <matemroz@gmail.com@5c68c409-c285-7ed9-41a2-fddc0483fbd3>
2011-04-03 11:09:45 +0000

    -poprawiona implementacja klasy Alarm, -dodana package Controller.Utils a w niej klasa Konwerter.

--- trunk/InteligentnyBudynek/src/Model/DAO/IUrzadzenieDAO.java
+++ trunk/InteligentnyBudynek/src/Model/DAO/IUrzadzenieDAO.java
@@ -1,35 +1,41 @@
 package Model.DAO;
 
- public interface IUrzadzenieDAO {
+public interface IUrzadzenieDAO {
 
     //zwraca z bazy idPokoju, w ktorym jest urzadzenie o danym idUrzadzenia
-     int znajdzUrzadzenie(int idUrzadzenia);
+    int znajdzUrzadzenie(int idUrzadzenia);
 
     //dodaje do bazy urzadzenie do danego pokoju (idUrzadzenia generowane jest automatycznie w bazie)
-     int dodaj(int idPokoju);
+    int dodaj(int idPokoju);
 
     //zmienia w bazie idPokoju do dla urzadzenia o danym idUrzadzenia
-     int przenies(int idUrzadzenia, int idPokoju);
+    int przenies(int idUrzadzenia, int idPokoju);
 
     //usuwa z bazy urzadzenie o danym idUrzadzenia
-     boolean usun(int idUrzadzenia) ;
+    boolean usun(int idUrzadzenia);
 
     //pobiera z bazy moc urzadzenia
-     double pobierzMoc(int idUrzadzenia);
+    double pobierzMoc(int idUrzadzenia);
 
     //wprowadza w bazie moc urzadzenia
-     double ustawMoc(int idUrzadzenia, double moc);
+    boolean ustawMoc(int idUrzadzenia, double moc);
 
-     double pobierzPoborGazu(int idUrzadzenia);
+    double pobierzPoborGazu(int idUrzadzenia);
 
-     boolean ustawPoborGazu(int idUrzadzenia, double poborGazu) ;
+    boolean ustawPoborGazu(int idUrzadzenia, double poborGazu);
 
     //rejestracja w bazie momentu wlaczenia urzadzenia
     boolean rejestrujWlaczenie(int idUrzadzenia);
 
-   //rejestracja w bazie momentu odlaczenia urzadzenia
+    //rejestracja w bazie momentu odlaczenia urzadzenia
     boolean rejestrujWylaczenie(int idUrzadzenia);
 
-   //pobranie z bazu czasu pracy urzadzenia
+    //pobranie z bazu czasu pracy urzadzenia
     int pobierzCzasPracy(int idUrzadzenia);
+
+    boolean czyPracuje(int idUrzadzenia);
+
+    String pobierzNazwa(int idUrzadzenia);
+
+    boolean ustawNazwa(int idUrzadzenia, String nazwa);
 }
